e89800e34e3cb5d486b35ada38104e3497ef7b94
Hyunju <jemmaa1@naver.com>
2019-04-07 22:12:32 +0900

    third commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hyunju\desktop\myapplication\myproject\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
